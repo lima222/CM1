9d0de9da002edff96c6b05f996d47437cb6c002a
DocAdmin <docadmin@Lab3711.local>
2018-04-27 13:49:59 +0100

    tableas

--- tabelas/tabelas/ViewController.swift
+++ tabelas/tabelas/ViewController.swift
@@ -8,21 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource {
+
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate {
     
-    var array = ["Lisboa", "Porto", "Viana"]
+    var array = ["Lisboa", "Porto", "Braga", "Viana do Castelo", "Aveiro"]
+    var arrayB = [false, false, false, false, false]
+    
+    //MARK: properties
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: actions
+    
+    @IBAction func butActionValidar(_ sender: Any) {
+        verifyChosen()
+    }
     
     
+    //MARK: ciclovida
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    //MARK: UITABLEVIEWDATASOURCE
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return array.count
@@ -31,10 +41,59 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "Cell")
         cell.textLabel?.text = array[indexPath.row]
-        cell.detailTextLabel?.text = "info adicional";
+        cell.detailTextLabel?.text = "info adicional"
+        
+        if arrayB[indexPath.row]{
+            cell.accessoryType = UITableViewCellAccessoryType.checkmark
+        }else {
+            cell.accessoryType = UITableViewCellAccessoryType.none
+        }
+        
         return cell
     }
-
-
+    
+    //MARK: UITABLEVIEWDELEGATE
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let editar = UITableViewRowAction(style: .default, title: "Editar"){action,index in
+            print("editar: " + String(index.row) + " " + self.array[index.row])
+        }
+        editar.backgroundColor = UIColor.blue
+        
+        let apagar = UITableViewRowAction(style: .default, title: "Apagar"){action,index in
+            print("apagar: " + String(index.row))
+        }
+        apagar.backgroundColor = UIColor.red
+        
+        return [editar, apagar]
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if arrayB[indexPath.row]{
+            tableView.cellForRow(at: indexPath as IndexPath)?.accessoryType = .none
+            arrayB[indexPath.row] = false
+        }else{
+            tableView.cellForRow(at: indexPath as IndexPath)?.accessoryType = .checkmark
+            arrayB[indexPath.row] = true
+        }
+        
+    }
+    
+    func verifyChosen(){
+        print("-----")
+        for index in 0...arrayB.count-1{
+            if arrayB[index]{
+                print(array[index])
+            }
+        }
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
 
