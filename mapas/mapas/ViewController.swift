77786d09914fa2bbb875ba5545eb20e6da54c15d
DocAdmin <docadmin@Lab3711.local>
2018-05-02 16:35:16 +0100

    mapas

--- mapas/mapas/ViewController.swift
+++ mapas/mapas/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController, MKMapViewDelegate {
+    
 
     @IBOutlet var mapView: MKMapView!
+    var geocoder = CLGeocoder()
+    @IBOutlet var labelGeo: UILabel!
+    @IBOutlet var textGeo: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +24,88 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let initialLocation = CLLocation(latitude: 41.701497, longitude: -8.834756)
         centerMapOnLocation(location: initialLocation)
         
+        let tapGesture  = UITapGestureRecognizer(target: self, action: #selector(self.tapped(_:)))
+        let longPressedRecognized = UILongPressGestureRecognizer(target: self, action: #selector(self.longPressed(_:)))
+        mapView.addGestureRecognizer(tapGesture)
+        mapView.addGestureRecognizer(longPressedRecognized)
+        
         
     }
+    @IBAction func butGeocoding(_ sender: Any) {
+        geocoder.geocodeAddressString(textGeo.text!) {
+            (placemarks, error) in self.processResponse(withPlacemarks: placemarks, error: error)
+        }
+    }
+    
+    private func processResponse(withPlacemarks placemarks :[CLPlacemark]?, error: Error? ){
+        
+        if let error = error {
+            print("aaa")
+            labelGeo.text = "Nao foi possivel encontrar a morada"
+            
+        } else {
+            var location :CLLocation?
+            if let placemarks = placemarks, placemarks.count>0{
+                location = placemarks.first?.location
+            }
+            if let location = location{
+                let coordinate = location.coordinate
+                labelGeo.text="\(coordinate.latitude),\(coordinate.longitude)"
+                centerMapOnLocation(location: CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                )
+                
+                
+            } else {
+                labelGeo.text = "nao foi possivel encontrar o endereco"
+            }
+        }
+    }
+    
+    private func processResponceRev(withPlacemarks placemarks : [CLPlacemark]?, error: Error?){
+        
+        if let error = error {
+            print("Unable to Reverse geocoding  Location(\(error))")
+            labelGeo.text="unable to find  address for location"
+            
+            
+        } else {
+            if let placemarks  = placemarks, let placemark = placemarks.first{
+                labelGeo.text = "\(placemark.country!) - \(placemark.locality!)  - \(placemark.postalCode!) - \(placemark.name!)"
+            }
+            else {
+                labelGeo.text = "no matching adress founfing"
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    func tapped(_ sender:UITapGestureRecognizer){
+        print("tapped")
+    }
+    
+    func longPressed(_ sender: UILongPressGestureRecognizer){
+        
+        if sender.state.rawValue == 1{
+            print("long tap")
+             let touchLocation = sender.location(in: mapView)
+             let locationCoordinate  = mapView.convert(touchLocation, toCoordinateFrom: mapView)
+             print("Long Tapped at lat:\(locationCoordinate.latitude) long: \(locationCoordinate.longitude)")
+            
+            let location = CLLocation(latitude: locationCoordinate.latitude, longitude: locationCoordinate.longitude)
+             
+             // geocode location
+             geocoder.reverseGeocodeLocation(location) {  (placemarks, error) in
+         
+             self.processResponceRev(withPlacemarks: placemarks, error: error)
+             
+             }
+            
+        }
+    }
+        
     
     func centerMapOnLocation(location: CLLocation) {
         let regionRadius: CLLocationDistance = 5000
