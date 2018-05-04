c8498de18369ed968bd73679cf53d7c522b28dc0
DocAdmin <docadmin@Lab3711.local>
2018-05-04 15:42:29 +0100

    mapas

---
+++ webservices/webservices/EntityReturnBook.swift
@@ -0,0 +1,18 @@
+//
+//  EntityReturnBook.swift
+//  webservices
+//
+//  Created by DocAdmin on 5/4/18.
+//  Copyright © 2018 ipvc.estg. All rights reserved.
+//
+
+import UIKit
+
+struct EntityReturnBook {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+
--- webservices/webservices/ViewController.swift
+++ webservices/webservices/ViewController.swift
@@ -21,5 +21,81 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func get(_ sender: Any) {
+        let urlString = "https://jsonplaceholder.typicode.com/posts/1"
+        /*guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let response = try JSONDecoder().decode(EntityReturnBook.self, from: data)
+                print("data:")
+                let jjsonBodyString = String(data: data, encoding: .utf8)
+                print("JSON String: ", jjsonBodyString)
+                DispatchQueue.main.async {
+                    //add
+                }
+            } catch let jsonError {
+                print(jsonError)
+            }
+        }.resume()*/
+        
+        /*let config = URLSessionConfiguration.default // Session Configuration
+        let session = URLSession(configuration: config) // Load configuration into Session
+        let url = URL(string: urlString)!
+        
+        let task = session.dataTask(with: url, completionHandler: {
+            (data, response, error) in
+            
+            if error != nil {
+                
+                print(error!.localizedDescription)
+                
+            } else {
+                
+                do {
+                    
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+                    {
+                        
+                        //Implement your logic
+                        print(json)
+                    }
+                    
+                } catch {
+                    print("error in JSONSerialization")
+                }
+                
+                
+            }
+            
+        })
+        task.resume()*/
+        var request = URLRequest(url: URL(string: urlString)!)
+        request.httpMethod = "POST"
+        let postString = "userId=1&title=123&body=limaamanha"
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+                
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+    }
 }
 
